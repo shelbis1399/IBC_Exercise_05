0d8786a41ab2ef74ea801782893a85beb8283d7a
lonken <lonken@nd.edu>
2019-10-08 15:03:38 -0400

    Solved part 2 of question 2

--- Assignment5.sh
+++ Assignment5.sh
@@ -1,8 +1,13 @@
-
+# This is the Exercise 5 answers.
+# Usage: bash Assignment5.sh "wages.csv"
 
 
 # This is the second part of the assignment
 # Returns the gender, yearsExperience, and wage for highest earner
+echo "HIGHEST WAGE EARNER:"
 cat "$1" | sort -t ',' -k 4 -n -r | head -n 1 | cut -d , -f 1,2,4
 
+# Returns the gender, yearsExperience, and wage for lowest earner
+echo "LOWEST WAGE EARNER:"
+cat "$1" | grep -v gender | sort -t , -k 4 -n | head -n 1 | cut -d , -f 1,2,4
 
