fd409fb2744a16bcbf9a6397565a122d5823c77c
lonken <lonken@nd.edu>
2019-10-08 15:54:52 -0400

    Began work on the third part. Having trouble adding up the column of wages

--- Assignment5.sh
+++ Assignment5.sh
@@ -14,3 +14,9 @@ cat "$1" | grep -v gender | sort -t , -k 4 -n | head -n 1 | cut -d , -f 1,2,4
 #Returns the number of females in the top ten earners
 echo "NUMBER OF FEMALES IN TOP 10 EARNERS:"
 cat "$1" | sort -t ',' -k 4 -n -r | head -n 10 | grep female | wc -l
+
+# This is the third part of the assignment
+# This greps those with 16 years experience
+collegenumber=$(cat "$1" | grep -v gender | sort -t , -k 4 -n | grep -E "[0-9],16," | wc -l)
+totalcollegewages=$(cat "$1" | grep -v gender | sort -t , -k 4 -n | grep -E "[0-9],16," | cut -d , -f 4 | tr '\n' '+' | sed 's/+$//' | bc)
+echo $totalcollegewages
