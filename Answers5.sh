197316d07bb0b7b246d4ffb99ad6cbd67a1369b3
lonken <lonken@nd.edu>
2019-10-10 19:43:55 -0400

    Question 2 Part 1

--- Answers5.sh
+++ Answers5.sh
@@ -5,4 +5,8 @@
 # Returns unique gender and years experience pairs in order and places them in a file
 cat "$1" | sed 's/,/ /g' | cut -d ' ' -f 1,2 | grep -v 'gender' | sort -k 1,1 -k 2,2n | uniq > genderyearsexperiences.txt
 
+#This is the second part of the assignment 
+# Returns the gender, yearsExperience, and wage for highest earner
+echo "HIGHEST WAGE EARNER:"
+echo "$1" | sort -t ',' -k 4 -n -r | head -n 1 | cut -d , -f 1,2,4
 
